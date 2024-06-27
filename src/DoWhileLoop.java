6054a3d53555695f37804ecd4b54103727da562f
Audyari Wiyono <audy123ari@gmail.com>
2024-06-27 20:19:13 +0700

    Do While Loop

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,16 @@
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+
+
+        var counter = 100;
+
+        do{
+            System.out.println("Perulangan " + counter);
+            counter++;
+        } while (counter <= 10);
+
+        System.out.println(counter);
+
+    }
+}
