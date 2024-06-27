e8c4a9704e8a6d7188aaabe84d900d1e1cfd29df
Audyari Wiyono <audy123ari@gmail.com>
2024-06-27 11:42:20 +0700

    Tipe Data Character

---
+++ src/TipeDataCharacter.java
@@ -0,0 +1,26 @@
+public class TipeDataCharacter {
+    public static void main(String[] args) {
+
+        char myChar = 'A';
+        System.out.println(myChar); // Output: A
+
+        char unicode = '\u0A12';
+        System.out.println(unicode); // Output:
+
+        int charAsInt = myChar;
+        System.out.println(charAsInt); // Output: 65
+
+        char charFromInt = (char) 65;
+        System.out.println(charFromInt); // Output: A
+
+
+        char e = 'E';
+        char k = 'K';
+        char o = 'O';
+
+        System.out.print(e);
+        System.out.print(k);
+        System.out.print(o);
+
+    }
+}
